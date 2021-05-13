edcc8730538b2f3c3ceeed33c3a11af09c8afc3b
Bobby Wasabi <bobby@gmail.com>
2021-05-12 18:44:21 -0700

    Wed, May 12, 2021

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour {
   public int forwardSpeed;
   public int sidewaysSpeed;
   public int upwardSpeed;
+  public Transform player;
 
     // FixedUpdate is better for physics stuff
     void FixedUpdate() {
@@ -17,10 +18,9 @@ public class PlayerMovement : MonoBehaviour {
       if (Input.GetKey("a")) {
         rb.AddForce(-sidewaysSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
       }
-      if (Input.GetKey("space")) {
+      if (Input.GetKey("space") && player.position.y < 1.01){
         rb.AddForce(0, upwardSpeed * Time.deltaTime, 0, ForceMode.VelocityChange);
       }
-
 
       if (rb.position.y < -1) {
         FindObjectOfType<GameManager>().GameOver();
